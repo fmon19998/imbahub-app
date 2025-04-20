bb26eaaf1d707bd79ed22ba235a72f0f9c1dcd6a
fmon19998 <fmon1998@gmail.com>
2025-04-20 20:30:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
 
-const express = require("express");
-const multer = require("multer");
-const cors = require("cors");
-const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -18,12 +12,15 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + "-" + file.originalname);
   }
 });
+
 const upload = multer({ storage: storage });
 
 app.post("/upload", upload.single("video"), (req, res) => {
   res.send("Video berhasil diupload!");
 });
 
-app.listen(port, () => {
-  console.log("IMBAHUB backend jalan di http://localhost:" + port);
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`IMBAHUB backend jalan di PORT ${PORT}`);
 });
